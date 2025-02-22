f4762641e3c3e4165b62871bf3c6987d8d1b32de
Abdalrahman Midani <55650856+aboodmidani@users.noreply.github.com>
2025-02-22 19:04:38 +0100

    Update whois_lookup.py

--- whois_lookup.py
+++ whois_lookup.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import whois
 
-def perform_whois_lookup(domain):
+def lookup_whois(domain):
     try:
         domain_info = whois.whois(domain)
-        return domain_info
-    except:
-        return "Invalid domain or WHOIS lookup failed."
-
+        return {
+            "Domain Name": domain_info.domain_name,
+            "Registrar": domain_info.registrar,
+            "WHOIS Server": domain_info.whois_server,
+            "Creation Date": domain_info.creation_date,
+            "Expiration Date": domain_info.expiration_date,
+            "Updated Date": domain_info.updated_date,
+            "Name Servers": domain_info.name_servers,
+            "Status": domain_info.status,
+            "Emails": domain_info.emails
+        }
+    except Exception as e:
+        return {"Error": str(e)}
